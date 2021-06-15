59e9ecfa16969eab79cbacae77b6333fc21a6fbe
madroid <madhusudhan.pro@gmail.com>
2021-06-15 20:24:02 +0530

    debug apk added code clean up

--- app/src/main/java/com/madroid/aisle/login/LoginActivity.java
+++ app/src/main/java/com/madroid/aisle/login/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Chronometer;
 
 import com.madroid.aisle.R;
